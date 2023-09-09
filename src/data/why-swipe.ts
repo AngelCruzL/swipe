a74cec1627afae66a7353e409869b9b42967876f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-09-09 12:57:48 -0600

    Add why swipe section

---
+++ src/data/why-swipe.ts
@@ -0,0 +1,32 @@
+type whySwipe = {
+  title: string;
+  description: string;
+  icon: string;
+};
+
+export const whySwipe: whySwipe[] = [
+  {
+    title: "Efficient Always",
+    description:
+      "We've optimized every level of the finance stack, from integrations to prebuilt checkout flows - no detail has been spared.",
+    icon: "/assets/cloud-icon.png",
+  },
+  {
+    title: "Fast-Improving",
+    description:
+      "We make constant updates and improvements to help you stay on the cutting edge of your industry, deploying to production around 20X per day!",
+    icon: "/assets/cycle-icon.png",
+  },
+  {
+    title: "Tried and true",
+    description:
+      "Our systems operate with 100% uptime, with scalability and uptime as a guarantee. Swipe is super duper certified so don't worry.",
+    icon: "/assets/shield-icon.png",
+  },
+  {
+    title: "Crazy Optimized",
+    description:
+      "We use machine learning trained on billions of data points to help optimize all products, from fraud to revenue recovery and more.",
+    icon: "/assets/bars-icon.png",
+  },
+];
