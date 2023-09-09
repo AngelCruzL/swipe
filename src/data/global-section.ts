130b1703f798f6573e9576b1351c6c097143cef8
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-09-09 15:34:12 -0600

    Add global section

---
+++ src/data/global-section.ts
@@ -0,0 +1,23 @@
+type globalSectionType = {
+  title: string;
+  description: string;
+};
+
+export const globalSectionData: globalSectionType[] = [
+  {
+    title: "250M+",
+    description: "API requests each day, peaking at 15,000 requests a second.",
+  },
+  {
+    title: "97%",
+    description: "of US adults have bought from businesses that use Swipe.",
+  },
+  {
+    title: "135+",
+    description: "currencies and payment methods supported",
+  },
+  {
+    title: "35+",
+    description: "countries with local, acquiring, optimizing acceptance rates",
+  },
+];
